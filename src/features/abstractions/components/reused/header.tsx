e5f087f399770472a8838abc4fab05f030ebc001
Mohammed Zaghloul <m.salahz86@gmail.com>
2025-12-04 19:01:30 +0200

    feat: update HeaderActions to use session prop and improve user greeting

--- src/features/abstractions/components/reused/header.tsx
+++ src/features/abstractions/components/reused/header.tsx
@@ -1,13 +1,9 @@
 import { Link } from '@tanstack/react-router'
 import { cn } from '@/features/abstractions/lib/utils'
-import { useSession } from '@/integrations/better-auth/auth-client'
+import { Session } from '@/integrations/better-auth/auth-client'
 import { Logo } from '@/features/abstractions/components/reused/logo'
 import { Theme } from '@/features/abstractions/components/reused/theme'
-import { Skeleton } from '@/features/abstractions/components/primitives/skeleton'
-import {
-  Button,
-  buttonVariants,
-} from '@/features/abstractions/components/primitives/button'
+import { Button } from '@/features/abstractions/components/primitives/button'
 
 export function Header(props: React.ComponentProps<'header'>) {
   return (
@@ -36,22 +32,24 @@ export function HeaderLogo(props: React.ComponentProps<'div'>) {
   )
 }
 
-export function HeaderActions(props: React.ComponentProps<'div'>) {
-  const { data, isPending } = useSession()
+export function HeaderActions({
+  session,
+  ...props
+}: React.ComponentProps<'div'> & { session: Session | null }) {
   return (
     <div
       data-slot="header-actions"
       className="flex items-center justify-end gap-2"
       {...props}
     >
-      {isPending ? (
-        <Skeleton className={cn(buttonVariants({ variant: 'outline' }))}>
-          <p className="invisible">Sign Out</p>
-        </Skeleton>
-      ) : data?.user ? (
-        <Button asChild variant="outline">
-          <Link to="/sign-out">Sign Out</Link>
-        </Button>
+      {session?.user ? (
+        <>
+          <span className="italic">Welcome back, {session.user.name}</span>
+
+          <Button asChild variant="outline">
+            <Link to="/sign-out">Sign Out</Link>
+          </Button>
+        </>
       ) : (
         <>
           <Button asChild variant="outline">
