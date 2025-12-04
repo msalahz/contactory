a96da8601e845a37ee9ec1c561adc29ffa8a08d8
Mohammed Zaghloul <m.salahz86@gmail.com>
2025-12-04 11:41:35 +0200

    feat: add isDevServerOnly function to check for development environment

--- src/features/abstractions/lib/utils.ts
+++ src/features/abstractions/lib/utils.ts
@@ -1,7 +1,15 @@
 import { clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+
 import type { ClassValue } from 'clsx'
+
+import { senv } from '@/env'
 
 export function cn(...inputs: Array<ClassValue>) {
   return twMerge(clsx(inputs))
+}
+
+// SERVER ONLY FUNCTION
+export function isDevServerOnly() {
+  return senv.ENV === 'development'
 }
