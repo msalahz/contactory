102f2115d25b3fef088bdb7015866cf0961ea02d
Mohammed Zaghloul <m.salahz86@gmail.com>
2025-12-06 01:45:14 +0200

    feat: add ENABLE_BETTER_AUTH_OPENAPI variable and improve environment variable documentation

--- src/env.ts
+++ src/env.ts
@@ -1,12 +1,16 @@
-import { createEnv } from '@t3-oss/env-core'
 import { z } from 'zod'
+import { createEnv } from '@t3-oss/env-core'
 
+/**
+ * The server-side environment variables, validated at runtime.
+ * These variables are only available on the server.
+ */
 export const senv = createEnv({
   server: {
-    ENV: z.enum(['development', 'production']).default('development'),
     DATABASE_URL: z.url(),
     BETTER_AUTH_URL: z.string(),
     BETTER_AUTH_SECRET: z.string(),
+    ENABLE_BETTER_AUTH_OPENAPI: z.boolean().default(false),
   },
   /**
    * What object holds the environment variables at runtime. This is usually
@@ -30,6 +34,10 @@ export const senv = createEnv({
   emptyStringAsUndefined: true,
 })
 
+/**
+ * The client-side environment variables, validated at runtime.
+ * These variables are only available on the client and prefixed with `VITE_`.
+ */
 export const cenv = createEnv({
   /**
    * The prefix that client-side variables must have. This is enforced both at
