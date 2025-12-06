c73d0078ec8e291842ea57a158576b4a73f6abd9
Mohammed Zaghloul <m.salahz86@gmail.com>
2025-12-06 22:49:57 +0200

    feat: add Netlify deployment support and update configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
 import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import { nitro } from 'nitro/vite'
+import { devtools } from '@tanstack/devtools-vite'
+import viteTsConfigPaths from 'vite-tsconfig-paths'
+import netlify from '@netlify/vite-plugin-tanstack-start'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
 const config = defineConfig({
   plugins: [
     devtools(),
-    nitro(),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
     tanstackStart(),
+    netlify(),
     viteReact({
       babel: {
         plugins: ['babel-plugin-react-compiler'],
