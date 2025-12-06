9ecb1224d011fc07e8ff841b9625d3a25013a377
Mohammed Zaghloul <m.salahz86@gmail.com>
2025-12-06 21:10:32 +0200

    feat: update noop function signature to support boolean return type

--- src/features/abstractions/lib/utils.ts
+++ src/features/abstractions/lib/utils.ts
@@ -9,7 +9,8 @@ export function cn(...inputs: Array<ClassValue>) {
 
 export function noop(): void
 export function noop(): Promise<void>
-export function noop(): void | Promise<void> {
+export function noop(): Promise<boolean>
+export function noop(): void | Promise<void | boolean> {
   // do nothing or return Promise.resolve() based on context
   return Promise.resolve()
 }
