1c5c40d3bc71a393404f474665791d45faf4c713
Mohammed Zaghloul <m.salahz86@gmail.com>
2025-12-06 21:14:52 +0200

    feat: implement forgot password route and associated form & hook

---
+++ src/routes/_auth/forgot-password.tsx
@@ -0,0 +1,46 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+import { AlertBox } from '@/features/abstractions/components/reused/alert-box'
+import { ItemTitle } from '@/features/abstractions/components/primitives/item'
+import { FieldError } from '@/features/abstractions/components/primitives/field'
+import { RequestPasswordResetForm } from '@/features/users/components/request-passowrd-reset-form'
+import { useRequestPasswordReset } from '@/integrations/better-auth/hooks/use-request-password-reset'
+
+export const Route = createFileRoute('/_auth/forgot-password')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  const { mutateAsync, data, error, isSuccess } = useRequestPasswordReset()
+
+  return (
+    <section className="flex min-h-full flex-col items-center justify-center p-6">
+      <RequestPasswordResetForm
+        onFormSubmit={async (data: { email: string }) => {
+          return mutateAsync({
+            email: data.email,
+            redirectTo: '/reset-password',
+          })
+            .then((result) => Promise.resolve(result?.status === true))
+            .catch(() => Promise.reject(false))
+        }}
+      >
+        {isSuccess ? (
+          <AlertBox type="success">
+            <ItemTitle>
+              {data?.message ||
+                'If an account with that email exists, check your email for a password reset link.'}
+            </ItemTitle>
+          </AlertBox>
+        ) : null}
+
+        {error ? (
+          <AlertBox type="error">
+            <ItemTitle>Request Failed</ItemTitle>
+            <FieldError errors={[error]} />
+          </AlertBox>
+        ) : null}
+      </RequestPasswordResetForm>
+    </section>
+  )
+}
