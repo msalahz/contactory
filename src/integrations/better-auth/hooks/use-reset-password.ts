06d849d016de7869b529c165ec6da71f5a4fb849
Mohammed Zaghloul <m.salahz86@gmail.com>
2025-12-06 21:15:03 +0200

    feat: implement reset password route and associated form & hook

---
+++ src/integrations/better-auth/hooks/use-reset-password.ts
@@ -0,0 +1,24 @@
+import { useMutation } from '@tanstack/react-query'
+import { authClient } from '@/integrations/better-auth/auth-client'
+
+export async function resetPassword(
+  props: Pick<
+    Parameters<typeof authClient.resetPassword>[0],
+    'newPassword' | 'token'
+  >,
+) {
+  const { data, error } = await authClient.resetPassword(props)
+
+  if (error) {
+    throw new Error(error.message, { cause: error })
+  }
+
+  return data
+}
+
+export function useResetPassword() {
+  return useMutation({
+    mutationKey: ['reset-password'],
+    mutationFn: resetPassword,
+  })
+}
---
+++ src/routes/_auth/reset-password.tsx
@@ -0,0 +1,55 @@
+import { z } from 'zod'
+import { createFileRoute } from '@tanstack/react-router'
+
+import { AlertBox } from '@/features/abstractions/components/reused/alert-box'
+import { ItemTitle } from '@/features/abstractions/components/primitives/item'
+import { FieldError } from '@/features/abstractions/components/primitives/field'
+import { ResetPasswordForm } from '@/features/users/components/reset-password-form'
+import { useResetPassword } from '@/integrations/better-auth/hooks/use-reset-password'
+
+export const Route = createFileRoute('/_auth/reset-password')({
+  validateSearch: z.object({
+    token: z.string().optional(),
+    error: z.literal('INVALID_TOKEN').optional(),
+  }),
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  const { token, error: invalidTokenError } = Route.useSearch()
+  const { mutateAsync, error, isSuccess } = useResetPassword()
+
+  return (
+    <section className="flex min-h-full flex-col items-center justify-center p-6">
+      <ResetPasswordForm
+        onFormSubmit={async (data: { newPassword: string }) => {
+          return mutateAsync({
+            newPassword: data.newPassword,
+            token,
+          })
+            .then((result) => Promise.resolve(result?.status === true))
+            .catch(() => Promise.reject(false))
+        }}
+      >
+        {!token || invalidTokenError ? (
+          <AlertBox type="error">
+            <ItemTitle>Invalid token</ItemTitle>
+          </AlertBox>
+        ) : null}
+
+        {isSuccess ? (
+          <AlertBox type="success">
+            <ItemTitle>Password reset successfully</ItemTitle>
+          </AlertBox>
+        ) : null}
+
+        {error ? (
+          <AlertBox type="error">
+            <ItemTitle>Reset password failed</ItemTitle>
+            <FieldError errors={[error]} />
+          </AlertBox>
+        ) : null}
+      </ResetPasswordForm>
+    </section>
+  )
+}
