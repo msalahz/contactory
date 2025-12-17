7773fd830d0565dca4f9c1e6f7b70d636c0717d3
Mohammed Zaghloul <m.salahz86@gmail.com>
2025-12-17 22:40:22 +0200

    feat(integrations): add Resend integration for email handling
    - Implemented core `sendEmail` function using the Resend SDK.
    - Added reusable components for `ResetPasswordEmail` and `VerifyEmail` email templates with responsive styles.
    - Updated `pnpm-lock.yaml` and `package.json` with required dependencies for Resend and @react-email components.

---
+++ src/integrations/resend/emails/verify-email.tsx
@@ -0,0 +1,62 @@
+import {
+  Body,
+  Button,
+  Container,
+  Head,
+  Hr,
+  Html,
+  Preview,
+  Section,
+  Tailwind,
+  Text,
+  pixelBasedPreset,
+} from '@react-email/components'
+
+import { senv } from '@/env'
+
+interface VerifyEmailProps {
+  name: string
+  url: string
+}
+
+const baseUrl = senv.BASE_URL ? `${senv.BASE_URL}` : ''
+
+export function VerifyEmail({ name, url }: VerifyEmailProps) {
+  return (
+    <Html>
+      <Head />
+      <Tailwind config={{ presets: [pixelBasedPreset] }}>
+        <Body className="font-koala bg-white">
+          <Preview>The intelligence platform that helps you manage your contacts.</Preview>
+          <Container className="mx-auto py-5 pb-12">
+            <Text className="text-base leading-[26px]">Hi {name},</Text>
+            <Text className="text-base leading-[26px]">
+              Click the button below to verify that this email address belongs to your account at{' '}
+              <a href={`${baseUrl}`}>Contactory</a>
+            </Text>
+            <Section className="text-center">
+              <Button
+                className="block rounded-[3px] bg-rose-500 p-3 text-center text-base text-white no-underline"
+                href={url}
+              >
+                Verify Email
+              </Button>
+              <Text className="text-muted-foreground text-sm leading-4">
+                The link will expire in 1 hours.
+              </Text>
+            </Section>
+            <Section className="text-center">
+              Button not working? Use this link instead: <a href={url}>Verify Email</a>
+            </Section>
+            <Text className="text-base leading-[26px]">
+              Best,
+              <br />
+              The Contactory team
+            </Text>
+            <Hr className="my-5 border-[#cccccc]" />
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  )
+}
---
+++ src/integrations/resend/resend.ts
@@ -0,0 +1,11 @@
+import { Resend } from 'resend'
+
+import type { CreateEmailOptions, CreateEmailRequestOptions } from 'resend'
+
+import { senv } from '@/env'
+
+const resend = new Resend(senv.RESEND_API_KEY)
+
+export function sendEmail(payload: CreateEmailOptions, options?: CreateEmailRequestOptions) {
+  return resend.emails.send(payload, options)
+}
