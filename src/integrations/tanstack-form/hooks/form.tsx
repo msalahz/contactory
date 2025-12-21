351a85931863783ea00f7f9c8fd71e6d19951326
Mohammed Zaghloul <m.salahz86@gmail.com>
2025-12-21 16:43:05 +0200

    feat(integrations): update `tanstack-form` hooks to include `SwitchField`, `SubmitButton`, and `ResetButton` components

--- src/integrations/tanstack-form/hooks/form.tsx
+++ src/integrations/tanstack-form/hooks/form.tsx
@@ -1,15 +1,19 @@
 import { createFormHook } from '@tanstack/react-form'
 
 import { InputField } from '@/integrations/tanstack-form/components/input-field'
-import { SubscribeButton } from '@/integrations/tanstack-form/components/subscribe-button'
+import { SwitchField } from '@/integrations/tanstack-form/components/swtch-field'
+import { ResetButton } from '@/integrations/tanstack-form/components/reset-button'
+import { SubmitButton } from '@/integrations/tanstack-form/components/submit-button'
 import { fieldContext, formContext } from '@/integrations/tanstack-form/hooks/form-context.tsx'
 
 export const { useAppForm, withForm, withFieldGroup } = createFormHook({
   fieldComponents: {
     Input: InputField,
+    Switch: SwitchField,
   },
   formComponents: {
-    SubscribeButton,
+    SubmitButton,
+    ResetButton,
   },
   fieldContext,
   formContext,
