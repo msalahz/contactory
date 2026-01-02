be33ac7f6681bfea41995ff36cb03b99512583bd
Mohammed Zaghloul <m.salahz86@gmail.com>
2026-01-02 22:44:03 +0200

    Revert "feat(config): add `wrangler.jsonc` configuration for Cloudflare Workers"
    This reverts commit 3bdf2beb3c03a9dcc9cb8689cff4a81c8f352b64.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import { devtools } from '@tanstack/devtools-vite'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
+import netlify from '@netlify/vite-plugin-tanstack-start'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import { cloudflare } from '@cloudflare/vite-plugin'
 
 const config = defineConfig({
   plugins: [
@@ -13,7 +13,7 @@ const config = defineConfig({
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
-    cloudflare({ viteEnvironment: { name: 'ssr' } }),
+    netlify(),
     tailwindcss(),
     tanstackStart(),
     viteReact({
