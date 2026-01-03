6c4a71204cea2572c200ab5496f881648680ed71
Mohammed Zaghloul <m.salahz86@gmail.com>
2026-01-03 23:00:38 +0200

    chore(auth): reorder imports in `auth.ts` for better consistency

--- src/server/schemas/auth.ts
+++ src/server/schemas/auth.ts
@@ -1,6 +1,6 @@
 import { relations } from 'drizzle-orm'
-import { boolean, index, pgTable, text, timestamp } from 'drizzle-orm/pg-core'
 import { createSelectSchema } from 'drizzle-zod'
+import { boolean, index, pgTable, text, timestamp } from 'drizzle-orm/pg-core'
 
 export const users = pgTable('users', {
   id: text('id').primaryKey(),
