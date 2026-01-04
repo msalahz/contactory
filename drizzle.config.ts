cac558816eb1079c2c7680438e5815640356021a
Mohammed Zaghloul <m.salahz86@gmail.com>
2026-01-04 02:55:10 +0200

    chore(config): remove non-null assertion in `DATABASE_URL` and fix formatting in `worker-configuration.d.ts`

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
   dialect: 'postgresql',
   casing: 'snake_case',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL,
   },
 })
