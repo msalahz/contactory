5e10ed6f93aebfc47315b022c0c03dfe88270b23
Mohammed Zaghloul <m.salahz86@gmail.com>
2026-01-06 19:08:49 +0200

    refactor(utils): remove `convertFileToBase64` utility and its tests
    - delete `convertFileToBase64` function and corresponding test suite as part of code cleanup

--- src/shared/utils/convertFileToBase64.ts
+++
@@ -1,8 +0,0 @@
-export function convertFileToBase64(file: File): Promise<string> {
-  return new Promise<string>((resolve, reject) => {
-    const reader = new FileReader()
-    reader.readAsDataURL(file) // Reads the file as a data URL (Base64)
-    reader.onload = () => resolve(reader.result as string) // Resolve with the Base64 string
-    reader.onerror = (error: ProgressEvent<FileReader>) => reject(error)
-  })
-}
