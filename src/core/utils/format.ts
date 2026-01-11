5f6c08ad044d6575b7f458dcc5a2c7e02143f06e
Mohammed Zaghloul <m.salahz86@gmail.com>
2026-01-11 23:27:04 +0200

    refactor(utils): remove unused `formatDate` function from `format.ts`

--- src/core/utils/format.ts
+++ src/core/utils/format.ts
@@ -4,14 +4,6 @@ export function getLanguageLocale(language: Language) {
   return language === 'en' ? 'en-US' : language === 'ar' ? 'ar-SA' : language
 }
 
-export function formatDate(date: Date, language: Language = 'en') {
-  return Intl.DateTimeFormat(getLanguageLocale(language), {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  }).format(date)
-}
-
 export function formatYear(date: Date, language: Language = 'en') {
   return Intl.DateTimeFormat(getLanguageLocale(language), {
     year: 'numeric',
